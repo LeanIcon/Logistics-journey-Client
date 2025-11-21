34c8c3376309ccbf7fe5cd466a17ec47bde00b30
D-IKE <d-ike@D-IKEs-MacBook-Air.local>
2025-11-21 19:57:37 +0000

    google analytics 2

--- plugins/ga.client.ts
+++ plugins/ga.client.ts
@@ -1,11 +1,10 @@
+// ~/plugins/ga.client.ts
 export default defineNuxtPlugin(() => {
-  // 1. Load the GA script
   const script = document.createElement("script");
   script.src = "https://www.googletagmanager.com/gtag/js?id=G-0Y26KC2S47";
   script.async = true;
 
   script.onload = () => {
-    // 2. Initialize GA
     window.dataLayer = window.dataLayer || [];
 
     function gtag(...args: any[]) {
@@ -14,19 +13,25 @@ export default defineNuxtPlugin(() => {
 
     window.gtag = gtag;
 
+    // Send initial page view
     gtag("js", new Date());
-    gtag("config", "G-0Y26KC2S47");
+    gtag("config", "G-0Y26KC2S47", {
+      page_path: window.location.pathname,
+    });
+
+    // Track SPA route changes
+    const router = useRouter();
+    router.afterEach((to) => {
+      if (typeof window.gtag === "function") {
+        window.gtag("config", "G-0Y26KC2S47", {
+          page_path: to.fullPath,
+        });
+      }
+    });
   };
 
   document.head.appendChild(script);
-
-  // 3. Track route changes
-  const router = useRouter();
-  router.afterEach((to) => {
-    if (typeof window.gtag === "function") {
-      window.gtag("config", "G-0Y26KC2S47", {
-        page_path: to.fullPath,
-      });
-    }
-  });
 });
+
+export {};
+
